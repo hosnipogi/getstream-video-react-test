fe36a6f95076dffc1bc0a31f32d02e6ae3a5be13
Hosni Bona <hosnibona.dev@gmail.com>
2023-08-31 05:17:20 +0800

    appdir init commit

---
+++ src/app/video/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ComponentVideo from "./_component";
+
+const Video = () => {
+  return (
+    <div>
+      <ComponentVideo />
+    </div>
+  );
+};
+
+export default Video;
